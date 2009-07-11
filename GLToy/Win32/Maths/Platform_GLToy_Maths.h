4130dcdb38b7255f967141f432cb803fe6b591f2
jheriko <none@none>
2009-07-11 11:34:42 +0000

    Missed Platform_GLToy_Maths.h on last update since it wasn't producing build errors...

--- GLToy/Win32/Maths/Platform_GLToy_Maths.h
+++ GLToy/Win32/Maths/Platform_GLToy_Maths.h
@@ -1,40 +1,4 @@
-#ifndef __GLTOY_MATHS_H_
-#define __GLTOY_MATHS_H_
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-// I N C L U D E S
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-#include "GLToy_Vector.h"
-
-#include "Maths/Platform_GLToy_Maths.h"
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-// C L A S S E S
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-class GLToy_Maths
-{
-
-public:
-    
-    static void Initialise();
-    static void Shutdown();
-
-    static float Clamp( float fValue, float fMin, float fMax ) { Min( fMax, Max( fValue, fMin ) ); }
-    static float Max( float fValue1, float fValue2 ) { return (fValue1 > fValue2) ? fValue1 : fValue2; }
-    static float Min( float fValue1, float fValue2 ) { return (fValue1 < fValue2) ? fValue1 : fValue2; }
-
-    static float Lerp( float fValue1, float fValue2, float fAmount ) { return fValue1 + ( fValue2 - fValue1 ) * fAmount; }
-    //static GLToy_Vector_2 Lerp( const GLToy_Vector_2& xValue1, const GLToy_Vector_2& xValue2, float fAmount ) { return xValue1 + ( xValue2 - xValue1 ) * fAmount; }
-    //static GLToy_Vector_3 Lerp( const GLToy_Vector_3& xValue1, const GLToy_Vector_3& xValue2, float fAmount ) { return xValue1 + ( xValue2 - xValue1 ) * fAmount; }
-    //static GLToy_Vector_4 Lerp( const GLToy_Vector_4& xValue1, const GLToy_Vector_4& xValue2, float fAmount ) { return xValue1 + ( xValue2 - xValue1 ) * fAmount; }
-
-private:
-    
-    static void Platform_Initialise();
-    static void Platform_Shutdown();
-
-};
+#ifndef __PLATFORM_GLTOY_MATHS_H_
+#define __PLATFORM_GLTOY_MATHS_H_
 
 #endif
