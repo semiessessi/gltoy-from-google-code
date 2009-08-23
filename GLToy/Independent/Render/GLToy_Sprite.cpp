f694ff0d9692f35a1da965eaf0ada5c4b9e738a6
jheriko <none@none>
2009-08-23 11:03:10 +0000

    Fix conflict

--- GLToy/Independent/Render/GLToy_Sprite.cpp
+++ GLToy/Independent/Render/GLToy_Sprite.cpp
@@ -6,33 +6,3 @@
 
 // This file's header
 #include <Render/GLToy_Sprite.h>
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-// F U N C T I O N S
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-GLToy_Sprite::GLToy_Sprite()
-: m_xPosition()
-, m_uTextureID( uGLTOY_BAD_HASH )
-{
-}
-
-GLToy_Sprite::~GLToy_Sprite()
-{
-}
-
-void GLToy_Sprite::SetPosition()
-{
-}
-
-void GLToy_Sprite::Initialise()
-{
-}
-
-void GLToy_Sprite::Shutdown()
-{
-}
-
-void GLToy_Sprite::Render()
-{
-}
