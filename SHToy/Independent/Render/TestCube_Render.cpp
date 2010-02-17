53888ddc632696fcb5a2f73f4dc22a49c435359d
jheriko <none@none>
2010-02-17 00:49:34 +0000

    Branch to play with spherical harmonics.

---
+++ SHToy/Independent/Render/TestCube_Render.cpp
@@ -0,0 +1,86 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// I N C L U D E S
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <Core/GLToy.h>
+
+// This file's header
+#include <Render/TestCube_Render.h>
+
+// GLToy
+#include <Core/GLToy_Timer.h>
+#include <Maths/GLToy_Vector.h>
+#include <Model/GLToy_Model_Coloured.h>
+#include <Render/GLToy_Render.h>
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// D A T A
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+GLToy_Model_Coloured TestCube_Render::s_xCube;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// F U N C T I O N S
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+bool TestCube_Render::Initialise()
+{
+    s_xCube.AddStripFromQuad(   GLToy_Vector_3( 1.0f, 1.0f, 1.0f ),
+                                GLToy_Vector_3( 1.0f, -1.0f, 1.0f ),
+                                GLToy_Vector_3( -1.0f, 1.0f, 1.0f ),
+                                GLToy_Vector_3( -1.0f, -1.0f, 1.0f ),
+                                GLToy_Vector_3( 1.0f, 0.0f, 0.0f )
+                                );
+
+    s_xCube.AddStripFromQuad(   GLToy_Vector_3( 1.0f, 1.0f, -1.0f ),
+                                GLToy_Vector_3( 1.0f, -1.0f, -1.0f ),
+                                GLToy_Vector_3( -1.0f, 1.0f, -1.0f ),
+                                GLToy_Vector_3( -1.0f, -1.0f, -1.0f ),
+                                GLToy_Vector_3( 0.0f, 1.0f, 0.0f )
+                                );
+
+    s_xCube.AddStripFromQuad(   GLToy_Vector_3( 1.0f, 1.0f, 1.0f ),
+                                GLToy_Vector_3( 1.0f, 1.0f, -1.0f ),
+                                GLToy_Vector_3( -1.0f, 1.0f, 1.0f ),
+                                GLToy_Vector_3( -1.0f, 1.0f, -1.0f ),
+                                GLToy_Vector_3( 0.0f, 0.0f, 1.0f )
+                                );
+    
+    s_xCube.AddStripFromQuad(   GLToy_Vector_3( 1.0f, -1.0f, 1.0f ),
+                                GLToy_Vector_3( 1.0f, -1.0f, -1.0f ),
+                                GLToy_Vector_3( -1.0f, -1.0f, 1.0f ),
+                                GLToy_Vector_3( -1.0f, -1.0f, -1.0f ),
+                                GLToy_Vector_3( 0.0f, 1.0f, 1.0f )
+                                );
+
+    s_xCube.AddStripFromQuad(   GLToy_Vector_3( 1.0f, 1.0f, 1.0f ),
+                                GLToy_Vector_3( 1.0f, 1.0f, -1.0f ),
+                                GLToy_Vector_3( 1.0f, -1.0f, 1.0f ),
+                                GLToy_Vector_3( 1.0f, -1.0f, -1.0f ),
+                                GLToy_Vector_3( 1.0f, 0.0f, 1.0f )
+                                );
+
+    s_xCube.AddStripFromQuad(   GLToy_Vector_3( -1.0f, 1.0f, 1.0f ),
+                                GLToy_Vector_3( -1.0f, 1.0f, -1.0f ),
+                                GLToy_Vector_3( -1.0f, -1.0f, 1.0f ),
+                                GLToy_Vector_3( -1.0f, -1.0f, -1.0f ),
+                                GLToy_Vector_3( 1.0f, 1.0f, 0.0f )
+                                );
+
+    return true;
+}
+
+void TestCube_Render::Shutdown()
+{
+}
+
+void TestCube_Render::Render()
+{
+    const float fTimer = 30.0f * GLToy_Timer::GetTime();
+    
+    GLToy_Render::Rotate( GLToy_Vector_3( 0.3f, 0.05f, -0.7f ), fTimer );
+    GLToy_Render::Rotate( GLToy_Vector_3( -0.7f, -0.05f, -0.3f ), 0.75f * fTimer );
+    GLToy_Render::Rotate( GLToy_Vector_3( 0.0f, 1.0f, 0.0f ), 0.25f * fTimer );
+
+    s_xCube.Render();
+}
