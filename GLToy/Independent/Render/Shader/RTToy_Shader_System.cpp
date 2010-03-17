0f7c82a640d8a80782255a2fb8d75b60682bfa82
jheriko <none@none>
2010-03-17 21:51:24 +0000

    Move RTToy_Shader_System into GLToy

---
+++ GLToy/Independent/Render/Shader/RTToy_Shader_System.cpp
@@ -0,0 +1,162 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// I N C L U D E S
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <Core/RTToy.h>
+
+// This file's header
+#include <Render/Shader/RTToy_Shader_System.h>
+
+// GLToy
+#include <Core/Data Structures/GLToy_HashTree.h>
+#include <File/GLToy_ANSITextFile.h>
+#include <File/GLToy_File_System.h>
+#include <Render/GLToy_Render.h>
+
+// RTToy
+#include <Render/Shader/RTToy_Shader.h>
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// D A T A
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+GLToy_HashTree< RTToy_ShaderProgram* > RTToy_Shader_System::s_xPrograms;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// F U N C T I O N S
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+bool RTToy_Shader_System::Initialise()
+{
+    s_xPrograms.Clear();
+
+    GLToy_Array< GLToy_String > xShaderPaths = GLToy_File_System::PathsFromFilter( "Shaders/", "*.shader" );
+
+    GLToy_ConstIterate( GLToy_String, xIterator, &xShaderPaths )
+    {
+        GLToy_String szName = xIterator.Current();
+        szName.RemoveAt( 0, 8 ); // remove "Shaders/"
+        szName.RemoveFromEnd( 7 ); // remove .shader
+    
+        GLToy_DebugOutput( "   - Found shader \"%S\".\r\n", szName.GetWideString() );
+        
+        GLToy_ANSITextFile xShaderFile = GLToy_ANSITextFile( xIterator.Current() );
+
+        GLToy_String szShaderData = xShaderFile.GetString();
+
+        const GLToy_String szFSPath = GLToy_String( "shaders/" ) + szShaderData.RemoveFirstLine() + ".fs";
+        const GLToy_String szVSPath = GLToy_String( "shaders/" ) + szShaderData.RemoveFirstLine() + ".vs";
+
+        GLToy_ANSITextFile xFSFile = GLToy_ANSITextFile( szFSPath );
+        GLToy_ANSITextFile xVSFile = GLToy_ANSITextFile( szVSPath );
+
+        if( xFSFile.GetSize() == 1 )
+        {
+            GLToy_DebugOutput( "   - Fragment shader \"%S\" is empty or doesn't exist.\r\n", szFSPath.GetWideString() );
+            continue;
+        }
+
+        if( xVSFile.GetSize() == 1 )
+        {
+            GLToy_DebugOutput( "   - Vertex shader \"%S\" is empty or doesn't exist.\r\n", szVSPath.GetWideString() );
+            continue;
+        }
+
+        const u_int uID = GLToy_Render::CreateProgram();
+        if( uID == 0 )
+        {
+            GLToy_DebugOutput( "   - Failed to create shader program \"%S\".\r\n", szName.GetWideString() );
+            continue;
+        }
+
+        const u_int uFSID = GLToy_Render::CreateFragmentShader();
+        if( uFSID == 0 )
+        {
+            GLToy_DebugOutput( "   - Failed to create fragment shader \"%S\".\r\n", szFSPath.GetWideString() );
+            continue;
+        }
+
+        const u_int uVSID = GLToy_Render::CreateVertexShader();
+        if( uVSID == 0 )
+        {
+            GLToy_DebugOutput( "   - Failed to create vertex shader \"%S\".\r\n", szVSPath.GetWideString() );
+            GLToy_Render::DeleteShader( uFSID );
+            continue;
+        }
+
+        char* pxFSString = new char[ xFSFile.GetSize() ];
+        xFSFile.GetRawString( pxFSString );
+
+        u_int uFoo = GLToy_Render::GetError();
+        bool bGoo = GLToy_Render::IsShader( uFSID );
+
+        GLToy_Render::ShaderSource( uFSID, 1, &pxFSString, 0 );
+        uFoo = GLToy_Render::GetError();
+        bGoo = GLToy_Render::IsShader( uFSID );
+        GLToy_Render::CompileShader( uFSID );
+        uFoo = GLToy_Render::GetError();
+        bGoo = GLToy_Render::IsShader( uFSID );
+
+        delete[] pxFSString;
+
+        char acLog[ 1024 ];
+        int iLogLength = 0;
+
+        GLToy_Render::GetShaderInfoLog( uFSID, 1023, &iLogLength, acLog );
+        uFoo = GLToy_Render::GetError();
+        bGoo = GLToy_Render::IsShader( uFSID );
+        if( iLogLength > 0 )
+        {
+            GLToy_DebugOutput( "   - Compiling fragment shader \"%S\"...\r\n", szFSPath.GetWideString() );
+            GLToy_DebugOutput( acLog );
+        }
+
+        char* pxVSString = new char[ xVSFile.GetSize() ];
+        xVSFile.GetRawString( pxVSString );
+
+        GLToy_Render::ShaderSource( uVSID, 1, &pxVSString, 0 );
+        GLToy_Render::CompileShader( uVSID );
+
+        delete[] pxVSString;
+
+        GLToy_Render::GetShaderInfoLog( uVSID, 1023, &iLogLength, acLog );
+        if( iLogLength > 0 )
+        {
+            GLToy_DebugOutput( "   - Compiling vertex shader \"%S\"...\r\n", szVSPath.GetWideString() );
+            GLToy_DebugOutput( acLog );
+        }
+
+        s_xPrograms.AddNode( new RTToy_ShaderProgram( uID, uFSID, uVSID ), szName.GetHash() );
+
+        GLToy_Render::ValidateProgram( uID );
+        GLToy_Render::GetProgramInfoLog( uID, 1023, &iLogLength, acLog );
+        if( iLogLength > 0 )
+        {
+            GLToy_DebugOutput( "   - Validating shader program \"%S\"...\r\n", szName.GetWideString() );
+            GLToy_DebugOutput( acLog );
+            GLToy_Render::DeleteShader( uFSID );
+        }
+    }
+
+    return true;
+}
+
+void RTToy_Shader_System::Shutdown()
+{
+    s_xPrograms.DeleteAll();
+    s_xPrograms.Clear();
+}
+
+void RTToy_Shader_System::BindShaderProgram( const GLToy_Hash uHash )
+{
+    RTToy_ShaderProgram** ppxShaderProgram = s_xPrograms.FindData( uHash );
+    if( ppxShaderProgram )
+    {
+        ( *ppxShaderProgram )->Bind();
+    }
+}
+
+void RTToy_Shader_System::BindShaderProgram( const GLToy_String& szName )
+{
+    return BindShaderProgram( szName.GetHash() );
+}
