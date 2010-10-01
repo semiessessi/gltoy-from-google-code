fb7319b5f2c0a4d50c176c164ddbc754e3d35f3d
jheriko <none@none>
2010-10-01 18:38:43 +0000

    GLToy: Branch to merge sound into GLToy

---
+++ GLToy/Win32/Sound/Platform_JD1_Sound_System.h
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// ©Copyright 2009, 2010 Semi Essessi
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __PLATFORM_JD1_SOUND_SYSTEM_H_
+#define __PLATFORM_JD1_SOUND_SYSTEM_H_
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// L I B R A R I E S
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma comment( lib, "openal32" )
+#pragma comment( lib, "alut" )
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// C L A S S E S
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+class Platform_JD1_Sound_System
+{
+
+public:
+
+    static bool Initialise();
+    static void Shutdown();
+
+    static void Update();
+
+    static GLToy_Handle CreateSoundHandle();
+    static GLToy_Handle CreateSourceHandle();
+
+    static void DestroySoundHandle( const GLToy_Handle iHandle );
+    static void DestroySourceHandle( const GLToy_Handle iHandle );
+
+    static void TestSound( const GLToy_Handle iHandle );
+
+protected:
+
+};
+
+#endif
