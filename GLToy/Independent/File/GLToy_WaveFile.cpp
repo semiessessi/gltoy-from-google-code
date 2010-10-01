cb7815bc4f8f7566e93b7e04d192be667b96596a
jheriko <none@none>
2010-10-01 19:00:46 +0000

    GLToy: Branch to merge sound into GLToy

---
+++ GLToy/Independent/File/GLToy_WaveFile.cpp
@@ -0,0 +1,57 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// ©Copyright 2010 Semi Essessi
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// I N C L U D E S
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <Core/GLToy.h>
+
+// This file's header
+#include <File/JD1_WaveFile.h>
+
+// JD1
+#include <Sound/JD1_Sound_System.h>
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// F U N C T I O N S
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+JD1_Sound* JD1_WaveFile::LoadSound() const
+{
+    if( m_pxSound )
+    {
+        return m_pxSound;
+    }
+
+    m_pxSound = new JD1_Sound();
+
+    m_pxSound->SetHandle( JD1_Sound_System::CreateSoundHandle() );
+
+    Platform_LoadSound( m_pxSound->GetHandle() );
+
+    return m_pxSound;
+}
