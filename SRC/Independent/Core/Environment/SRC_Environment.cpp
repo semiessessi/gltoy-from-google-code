3b71d91bb32e7ff5b347a084ff3e05399739784f
PAK.Nine@gmail.com <PAK.Nine@gmail.com>
2010-11-11 21:45:12 +0000

    Environment and map classes

---
+++ SRC/Independent/Core/Environment/SRC_Environment.cpp
@@ -0,0 +1,61 @@
+
+// Thomas Young
+
+#include "Core/GLToy.h"
+
+#include "SRC_Environment.h"
+
+#include "Maths/GLToy_Plane.h"
+
+SRC_Environment( const GLToy_Plane& xPlane, const GLToy_String& szTextureName )
+: GLToy_Environment_Plane( xPlane, szTextureName ) 
+{
+}
+
+SRC_Environment::~SRC_Environment()
+{
+}
+
+void SRC_Environment::Initialise()
+{
+	GLToy_Parent::Initialise();
+}
+
+void SRC_Environment::Update()
+{
+	GLToy_Parent::Update();
+}
+
+void SRC_Environment::Render()
+{
+	GLToy_Parent::Render();
+}
+		
+void SRC_Environment::Shutdown()
+{
+	GLToy_Parent::Shutdown();
+}
+
+int SRC_Environment::GetType() const
+{
+	// huh? return a 'type', the enumeration for which is in engine code o_O
+	return 0xDeadBeef;
+}
+
+void SRC_Environment::ReadFromBitStream( const GLToy_BitStream& xStream )
+{
+	GLToy_Parent::ReadFromBitStream( xStream );
+}
+
+void SRC_Environment::WriteToBitStream( GLToy_BitStream& xStream ) const
+{
+	GLToy_Parent::WriteToBitStream( xStream );
+}
+
+float SRC_Environment::Trace( const GLToy_Ray& xRay, const float fLimitingDistance ) const
+{
+	// TODO: Cut a square out of a plane and make it the correct dimensions for the map blocks
+	// also, make this use the depth buffer if it does not already
+
+	GLToy_Parent::Trace(xRay, fLimitingDistance);
+}
---
+++ SRC/Independent/Core/Environment/SRC_Environment.h
@@ -0,0 +1,41 @@
+
+// Thomas Young
+
+#ifndef _SRC_ENVIRONMENT_
+#define _SRC_ENVIRONMENT_
+
+#include "Environment/GLToy_Environment_Plane.h"
+
+// Dear Semi, the word Environment is very long to type, I do not enjoy having to type it a lot
+// in future please use a nice short word like 'World' or 'Scene', thank you - Tom
+
+class GLToy_Plane;
+
+class SRC_Environment : public GLToy_Environment_Plane
+{
+	typedef GLToy_Environment_Plane GLToy_Parent;
+
+	public:
+
+		SRC_Environment( const GLToy_Plane& xPlane, const GLToy_String& szTextureName )
+			: GLToy_Environment_Plane( xPlane, szTextureName ) {}
+		~SRC_Environment();
+
+		void Initialise();
+
+		void Update();
+		void Render();
+		
+		void Shutdown();
+
+		void ReadFromBitStream( const GLToy_BitStream& xStream );
+		void WriteToBitStream( GLToy_BitStream& xStream ) const;
+
+		int GetType() const;
+
+		float Trace( const GLToy_Ray& xRay, const float fLimitingDistance ) const;		
+};
+
+#endif
+
+// eof
