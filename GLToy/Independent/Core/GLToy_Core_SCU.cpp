4440b1c7d8cc036b78e82f842affa9c151f2be4e
jheriko <none@none>
2010-12-24 22:40:13 +0000

    GLToy: Core SCU

---
+++ GLToy/Independent/Core/GLToy_Core_SCU.cpp
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// ©Copyright 2010 Semi Essessi
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// I N C L U D E S
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <Core/Console/GLToy_Console.cpp>
+#include <Core/Console/GLToy_ConsoleCommand.cpp>
+#include <Core/Console/GLToy_ConsoleVariable.cpp>
+#include <Core/Data Structures/GLToy_BitStream.cpp>
+#include <Core/State/GLToy_State_Editor.cpp>
+#include <Core/State/GLToy_State_EditorFrontEnd.cpp>
+#include <Core/State/GLToy_State_FixedCamera.cpp>
+#include <Core/State/GLToy_State_Splash.cpp>
+#include <Core/State/GLToy_State_System.cpp>
+#include <Core/GLToy_Assert.cpp>
+#include <Core/GLToy_Memory.cpp>
+#include <Core/GLToy_Timer.cpp>
