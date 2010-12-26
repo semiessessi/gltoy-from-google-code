4ccd4ee59e0fb9fd6cef3c8ebe3980e4469915ac
jheriko@gmail.com <jheriko@gmail.com>
2010-12-26 21:01:39 +0000

    GLToy: Entity SCU

---
+++ GLToy/Independent/Entity/GLToy_Entity_SCU.cpp
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// ©Copyright 2010 Semi Essessi
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// I N C L U D E S
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <Entity/BSP/Common/GLToy_Entity_BSP_Func_Button.cpp>
+#include <Entity/BSP/Common/GLToy_Entity_BSP_Info_Player_Start.cpp>
+#include <Entity/BSP/Common/GLToy_Entity_BSP_Trigger_Counter.cpp>
+#include <Entity/BSP/Quake 2/GLToy_Entity_BSP_Target_ChangeLevel.cpp>
+#include <Entity/Model/GLToy_Entity_ModelAnimated.cpp>
+#include <Entity/Model/GLToy_Entity_ModelStatic.cpp>
+#include <Entity/Physics/GLToy_Entity_PhysicsBox.cpp>
+#include <Entity/Sprite/GLToy_Entity_Sprite.cpp>
+#include <Entity/GLToy_Entity.cpp>
+#include <Entity/GLToy_Entity_System.cpp>
