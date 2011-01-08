9f29ff842ad8abbcb52fdcbe15f86f15dcd5ade4
jheriko <none@none>
2011-01-08 19:54:00 +0000

    GLToy: Broken point lights - need to work out why depth texture sampling is failing so spectacularly

--- Data/Shaders/Light_Point.fs
+++ Data/Shaders/Light_Point.fs
@@ -1,8 +1,26 @@
 uniform sampler2D xDiffuseSampler;
 uniform sampler2D xNormalSampler;
+uniform sampler2D xDepthSampler;
+
+uniform float fLightRadius;
+uniform vec3 xCameraPosition;
+uniform vec3 xLightPosition;
+uniform vec3 xLightColour;
+// this view vector is not good enough... it should be a varying
+uniform vec3 xViewVector;
+
+uniform vec2 xSize;
+uniform vec2 xOneOverSize;
 
 void main()
 {
-	// TODO: ...
-	gl_FragColor = vec4( 0.0, 0.0, 0.0, 1.0 );
+	// TODO: fix this up
+	vec3 xDiffuse = texture2D( xDiffuseSampler, gl_FragCoord.xy * xOneOverSize ).xyz;
+	// this depth value is bollocks
+	vec3 xPosition = xCameraPosition + xViewVector * texture2D( xDepthSampler, gl_FragCoord.xy * xOneOverSize ).x;
+	vec3 xLightVector = xPosition - xLightPosition;
+	float fAttenuation = 1.0f - dot( xLightVector, xLightVector ) / ( fLightRadius * fLightRadius );
+	float fNormalDot = 1.0; // TODO...
+	vec3 xColour = xLightColour * fAttenuation * fNormalDot * xDiffuse;
+	gl_FragColor = vec4( xColour, 1.0 );
 }
